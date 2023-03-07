6de153fec0ad3e57fe81b66d939952d9672f795d
shehzad-kore <sibrahim@korewireless.com>
2023-03-07 10:29:59 +0530

    first commit

---
+++ OmniCore/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from OmniCore.api.device_api import DeviceApi
+from OmniCore.api.registry_api import RegistryApi
